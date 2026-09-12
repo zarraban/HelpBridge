ba752854f77f2ec271dfe36b217a0bf28c7bce71
Smyrnov Vlad <vladsimovv@gmail.com>
2026-09-12 22:01:56 +0300

    feat: add repo architecture, dependencies

---
+++ src/main/java/com/example/help_bridge/HelpBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.help_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpBridgeApplication.class, args);
+	}
+
+}
