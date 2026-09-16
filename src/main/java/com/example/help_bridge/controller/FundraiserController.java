194fab3111a3d199e6b948abfa5801d22ecf9330
Smyrnov Vlad <vladsimovv@gmail.com>
2026-09-16 11:37:34 +0300

    feat: add FundraiserController, related dtos

---
+++ src/main/java/com/example/help_bridge/controller/FundraiserController.java
@@ -0,0 +1,39 @@
+package com.example.help_bridge.controller;
+
+import com.example.help_bridge.dto.request.SendMailingRequest;
+import com.example.help_bridge.dto.response.FundraiserResponse;
+import com.example.help_bridge.dto.response.SendMailingResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+// TODO When using Thymeleaf will be converted to @Controller
+@RestController
+@RequestMapping("/api/fundraiser")
+public class FundraiserController {
+
+    @GetMapping("/id")
+    public ResponseEntity<FundraiserResponse> getFundraiserById(){
+        return ResponseEntity.ok(null);
+    }
+
+    // TODO add field which will be used to sort by
+    @GetMapping
+    public ResponseEntity<List<FundraiserResponse>> getAllFundraisers(
+            @RequestParam(value = "sort", defaultValue = "asc") String sort,
+            @RequestParam(value = "page", defaultValue = "1") Long page,
+            @RequestParam(value = "size", defaultValue = "5") Long size
+    ){
+        return ResponseEntity.ok(Collections.emptyList());
+    }
+
+    @GetMapping
+    public ResponseEntity<SendMailingResponse> sendMailToDonors(
+            @RequestBody @Valid SendMailingRequest request
+            ){
+        return ResponseEntity.ok(null);
+    }
+}
