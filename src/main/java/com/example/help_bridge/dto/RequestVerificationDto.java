7267f8bd7172a91a29b02c6140389055853cfdd8
Sanna001 <sannaafanasieva@gmail.com>
2026-09-16 21:33:15 +0300

    Update RequestVerificationDto.java

--- src/main/java/com/example/help_bridge/dto/RequestVerificationDto.java
+++ src/main/java/com/example/help_bridge/dto/RequestVerificationDto.java
@@ -2,7 +2,7 @@ package com.example.help_bridge.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record RequestAdminReviewRequest(
+public record RequestVerificationDto(
         @NotNull(message = "Decision (approved/rejected) is mandatory")
         Boolean approved,
 
