1c80a66f69afde851c95b8b1135c9847caa5c4d1
Sanna001 <sannaafanasieva@gmail.com>
2026-09-16 21:25:58 +0300

    Update RequestBookingDto.java

--- src/main/java/com/example/help_bridge/dto/RequestBookingDto.java
+++ src/main/java/com/example/help_bridge/dto/RequestBookingDto.java
@@ -2,10 +2,7 @@ package com.example.help_bridge.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public class RequestBookingDto {
-
-    public record BookRequestRequest(
-            @NotNull(message = "Fund ID is mandatory")
-            Long fundId
-    ) {}
-}
+public record RequestBookingDto(
+        @NotNull(message = "Fund ID is mandatory")
+        Long fundId
+) {}
