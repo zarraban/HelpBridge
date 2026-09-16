946bb25905b6a2fffaa145c3b7501d5e7451144b
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-09-16 14:27:50 +0300

    feat: add VolunteerStatistics related dtos, service, controller and test

---
+++ src/main/java/com/example/help_bridge/dto/VolunteerStatisticsRequest.java
@@ -0,0 +1,20 @@
+package com.example.help_bridge.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+//Query parameters ?from=YYYY-MM-DD&to=YYYY-MM-DD
+public record VolunteerStatisticsRequest(
+        @NotNull(message = "Parameter 'from' is required")
+        @PastOrPresent(message = "Parameter 'from' cannot be in the future")
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        LocalDate from,
+        @NotNull(message = "Parameter 'to' is required")
+        @PastOrPresent(message = "Parameter 'to' cannot be in the future")
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        LocalDate to
+) {
+}
