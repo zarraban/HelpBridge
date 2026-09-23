729005890647c58cab7dd2a73570ddaed16f4799
Smyrnov Vlad <vladsimovv@gmail.com>
2026-09-23 20:06:34 +0300

    feat: implement strategy pattern for validating different types of Evidences

---
+++ src/main/java/com/example/help_bridge/fundraiser/strategy/FeedbackValidatorStrategy.java
@@ -0,0 +1,19 @@
+package com.example.help_bridge.fundraiser.strategy;
+
+import com.example.help_bridge.common.exception.InvalidEvidenceException;
+import com.example.help_bridge.fundraiser.entity.Evidence;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeedbackValidatorStrategy implements EvidenceValidatorStrategy{
+    @Override
+    public boolean supports(Evidence evidence) {
+        return evidence.getAttachmentUrl() != null && !evidence.getAttachmentUrl().isBlank();    }
+
+    @Override
+    public void validate(Evidence evidence) {
+        if (evidence.getRecipientFeedback().length() > 1000) {
+            throw new InvalidEvidenceException("Feedback is too long!");
+        }
+    }
+}
---
+++ src/main/java/com/example/help_bridge/fundraiser/strategy/PhotoValidatorStrategy.java
@@ -0,0 +1,23 @@
+package com.example.help_bridge.fundraiser.strategy;
+
+import com.example.help_bridge.common.exception.InvalidEvidenceException;
+import com.example.help_bridge.fundraiser.entity.Evidence;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PhotoValidatorStrategy implements EvidenceValidatorStrategy{
+    @Override
+    public boolean supports(Evidence evidence) {
+        return evidence.getAttachmentUrl() != null && !evidence.getAttachmentUrl().isBlank();    }
+
+    @Override
+    public void validate(Evidence evidence) {
+        String url = evidence.getAttachmentUrl();
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            throw new InvalidEvidenceException("Url link for photo should start from http:// or https://!");
+        }
+        if (!url.endsWith(".jpg") && !url.endsWith(".jpeg") && !url.endsWith(".png")) {
+            throw new InvalidEvidenceException("Photos are allowed in following formats only: .jpg, .jpeg or .png!");
+        }
+    }
+}
