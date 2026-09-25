8e34f5c0305bb457663f7799f7160402a265aa9a
Дарʼя Чорна <dariachorna@MacBook-Air-Dara.local>
2026-09-25 13:20:53 +0300

    Changed notificationEventListener

--- src/main/java/com/example/help_bridge/fund/notification/FundStatusNotificationListener.java
+++ src/main/java/com/example/help_bridge/fund/notification/FundStatusNotificationListener.java
@@ -3,7 +3,8 @@ package com.example.help_bridge.fund.notification;
 import com.example.help_bridge.fund.event.FundStatusChangedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,9 +12,10 @@ public class FundStatusNotificationListener {
 
     private static final Logger log = LoggerFactory.getLogger(FundStatusNotificationListener.class);
 
-    @ApplicationModuleListener
+    @EventListener
+    @Async
     void on(FundStatusChangedEvent event) {
-        log.info("Async notification: fund [{}] status changed {} -> {}",
-                event.fundId(), event.previousStatus(), event.newStatus());
+        log.info("Async notification: fund [" + event.fundId() + "] status changed {} -> {}",
+                 event.previousStatus(), event.newStatus());
     }
 }
